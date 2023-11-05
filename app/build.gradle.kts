bacb3c7ba84210317f2c15b65a8df999a2e1a7e3
beom <cjja00@konkuk.ac.kr>
2023-11-06 00:28:12 +0900

    navigation bar수정,floating button 위치 오류

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,9 +39,12 @@ android {
 }
 
 dependencies {
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    val fragment_version = "1.6.2"
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
