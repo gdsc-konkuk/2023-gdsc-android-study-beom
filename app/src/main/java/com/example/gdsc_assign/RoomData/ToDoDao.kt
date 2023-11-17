c2accdb67c7c28e81154e562c1b8c7b059f930c1
HyeseonBaek <konkukbhs@gmail.com>
2023-11-18 02:01:23 +0900

    @Dao annotation 추가

--- app/src/main/java/com/example/gdsc_assign/RoomData/ToDoDao.kt
+++ app/src/main/java/com/example/gdsc_assign/RoomData/ToDoDao.kt
@@ -1,9 +1,11 @@
 package com.example.gdsc_assign.RoomData
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
+@Dao
 interface ToDoDao {
     @Insert
     fun insert(user: ToDo)
@@ -14,10 +16,10 @@ interface ToDoDao {
     @Query("SELECT * FROM ToDo")
     fun selectAll(): List<ToDo>
 
-    @Query("SELECT * FROM ToDo WHERE todoid =  todoid")
-    fun selectByToDoId(todoid: Int): ToDo
-
-    @Query("UPDATE ToDo SET body = :name  WHERE todoid = :name")
-    fun updateNameByUserId(userId: Int, name: String)
+//    @Query("SELECT * FROM ToDo WHERE todoid =  todoid")
+//    fun selectByToDoId(todoid: Int): ToDo
+//
+//    @Query("UPDATE ToDo SET body = :name  WHERE todoid = :name")
+//    fun updateNameByUserId(userId: Int, name: String)
 
 }
